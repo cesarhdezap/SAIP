3e9b9e894474f0ed2613c71533c0dddd1f006ea4
Josué Alejandro Díaz Rojas <Patchinster@Gmail.com>
2020-04-16 12:24:06 -0500

    Adición del modelo de datos, clases de dominio preliminares y pantallas preliminares

---
+++ SAIP/InterfazDeUsuario/CallCenter/GUIPedidoADomicilio.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using LogicaDeNegocio.Clases;
+using LogicaDeNegocio.ObjetosAccesoADatos;
+
+namespace InterfazDeUsuario.CallCenter
+{
+	/// <summary>
+	/// Interaction logic for GUIPedidoADomicilio.xaml
+	/// </summary>
+	public partial class GUIPedidoADomicilio : Window
+	{
+
+		public Empleado EmpleadoDeCallCenter { get; set; } = new Empleado();
+		public Iva Iva { get; set; } = new Iva();
+		public GUIPedidoADomicilio(Empleado EmpleadoDeCallCenter)
+		{
+			InitializeComponent();
+			this.EmpleadoDeCallCenter = EmpleadoDeCallCenter;
+			NombreDeUsuarioLabel.Content = EmpleadoDeCallCenter.Nombre;
+			IvaDAO ivaDAO = new IvaDAO();
+			Iva = ivaDAO.CargarIvaActual();
+			
+			DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0,0,1), DispatcherPriority.Normal, delegate
+			{
+				HoraLabel.Content = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+			}, this.Dispatcher);
+			HoraLabel.Content = DateTime.Now.ToString("MM/dd/yyyy hh:mm tt");
+		}
+	}
+}
---
+++ SAIP/LogicaDeNegocio/Clases/Iva.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio.Clases
+{
+	public class Iva
+	{
+        public int Id { get; set; }
+        public DateTime FechaDeInicio { get; set; }
+        public double Valor { get; set; }
+        public string Creador { get; set; }
+        public DateTime FechaDeCreacion { get; set; }
+        public bool Activo { get; set; }
+    }
+}
