395a1bf12537d10cdc414529549c3f885067379a
ARTURO VILLA LOPEZ <zS17012946@estudiantes.uv.mx>
2020-04-22 02:13:39 -0500

    Se agregan clases del dominio faltantes "Cliente" y "Cuenta"

---
+++ SAIP/LogicaDeNegocio/Clases/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio.Clases
+{
+    public class Cliente
+    {
+        public int Id { get; set;}
+        public string Nombre { get; set;}
+        public string Telefono { get; set;}
+        public List<string> Direcciones { get; set;}
+        public string Comentario { get; set;}
+        public Cuenta Cuenta { get; set;}
+    }
+}
---
+++ SAIP/LogicaDeNegocio/Clases/Cuenta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio.Clases
+{
+    public class Cuenta
+    {
+        public int Id { get; set;}
+        public string Direccion { get; set;}
+        public int PrecioTotal { get; set;}
+        public Cliente Cliente { get; set;}
+
+        public void AñadirPedido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
